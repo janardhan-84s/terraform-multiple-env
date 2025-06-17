29be6c5cd161cb085027957bc199c64e0d23c767
Janardhan <janardhanj265@gmail.com>
2025-06-17 15:35:23 +0530

    workspaces

---
+++ workspaces/provider.tf
@@ -0,0 +1,24 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "jana-remote-state"
+    key    = "workspace-demo"
+    region = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+     region = "us-east-1"
+  # Configuration options
+}
+
+
